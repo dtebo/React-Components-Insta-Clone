5bc4d5183241bd8ff7671ee637b3d58ba58670cf
Darren Tebo <darrentebo83@outlook.com>
2020-05-22 21:27:52 -0400

    finished styling app

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -3,14 +3,19 @@ import React from 'react';
 
 const CommentInput = props => {
   return (
-    <form className="comment-form" 
-          onSubmit={(e) => {
-            props.submitComment({username: "dtebo", text: e.target[0].value});
-          }}>
+    <form className="comment-form">
       <input
         type="text"
         value={props.comment}
         placeholder="Add comment... "
+        onKeyDown={(e) => {
+          if(e.keyCode === 13){
+            props.submitComment({username: "dtebo", text: e.target.value});
+          }
+          else{
+            e.target.value += e.key.toString();
+          }
+        }}
       />
     </form>
   );
